13b9cd9b3ca1ffe91efb8711e5f26c994690bd6b
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-26 13:32:31 -0700

    Installed prisma with prisma-session-store

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import session from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 
 const app = express();
+
+app.use(
+    session({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        },
+        secret: "cats",
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+            prisma,
+            {
+                checkPeriod: 2 * 60 * 1000,
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined
+            }
+        )
+    })
+)
 
 const PORT = process.env.PORT || 5051;
 
